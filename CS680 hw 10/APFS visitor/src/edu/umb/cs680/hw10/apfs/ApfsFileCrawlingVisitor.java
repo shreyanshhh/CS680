97a2fd434a87238091dec4924f3c524e8d351c9c
shreyanshhh <60211668+shreyanshhh@users.noreply.github.com>
2021-05-20 22:59:42 +0530

    Add files via upload

---
+++ CS680 hw 10/APFS visitor/src/edu/umb/cs680/hw10/apfs/ApfsFileCrawlingVisitor.java
@@ -0,0 +1,31 @@
+package edu.umb.cs680.hw10.apfs;
+
+import java.util.LinkedList;
+
+public class ApfsFileCrawlingVisitor implements ApfsVisitor{
+
+	private LinkedList<ApfsFile> files =  new LinkedList<ApfsFile>();
+	
+	@Override
+	public void visit(ApfsLink link) {
+		return;
+	}
+
+	@Override
+	public void visit(ApfsDirectory dir) {
+		return;
+	}
+
+	@Override
+	public void visit(ApfsFile file) {
+		/*if(null == files) {
+			files = new LinkedList<ApfsFile>();
+		}*/
+		this.files.add(file);
+	}
+	
+	public LinkedList<ApfsFile> getFiles() {
+		return this.files;
+	}
+
+}
